6ec9deac997546a0ddac9f99a22d23ff3354182e
maevet78 <190439mt@saintviator.com>
2018-08-09 20:54:40 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -42,7 +42,7 @@ def convert_temp(weather)
   x = weather[:temperature_farenheit] - 32.0
   celsius = x * (5.0/9.0)
   celsius = celsius.round(2)
-  puts "The temperature in celsius is #{celsius}°C."
+  return "The temperature in celsius is #{celsius}°C."
 end 
 
 puts convert_temp(weather)
