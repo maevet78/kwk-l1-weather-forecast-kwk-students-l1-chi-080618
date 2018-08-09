ccd812ba0e3c6a7794b51b98f9861e51d7c0b669
maevet78 <190439mt@saintviator.com>
2018-08-09 20:46:50 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -41,7 +41,7 @@ city_weather_info(weather)
 def convert_temp(weather)
   x = weather[3] - 32
   celsius = x * (5/9)
-  puts "The temperature in celsius is #{celsius} 
+  puts "The temperature in celsius is #{celsius}°C."
 end 
 
 # Hint - how can we find the algorithm to convert Farenheit to Celsius?
